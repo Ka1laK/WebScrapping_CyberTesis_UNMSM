afc9a8d911085f2ea46424091762b0b42a3253e5
Ka1laK <rissel1771@gmail.com>
2024-11-19 12:28:51 -0500

    feat:WS_Scripts

--- scripts/scraper.py
+++ scripts/scraper.py
@@ -48,29 +48,29 @@ autores = []
 
 while True:
     # Extraer los titulos
-    titulos_elements = driver.find_elements(By.XPATH, "//a[contains(@class, 'MuiTypography-h5')]")
-    titulos.extend([titulo.text for titulo in titulos_elements])
+    encontrar_titulos = driver.find_elements(By.XPATH, "//a[contains(@class, 'MuiTypography-h5')]")
+    titulos.extend([titulo.text for titulo in encontrar_titulos])
 
     # Extraer los anios
-    anios_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'css-gg4vpm')]")
-    anios.extend([anio.text.split()[-1] for anio in anios_elements])
+    encontrar_anios = driver.find_elements(By.XPATH, "//div[contains(@class, 'css-gg4vpm')]")
+    anios.extend([anio.text.split()[-1] for anio in encontrar_anios])
 
     # Extraer los autores
-    autores_elements = driver.find_elements(By.XPATH, "//div[contains(@class, 'MuiStack-root css-kvb41a')]")
-    for autor in autores_elements:
+    encontrar_autores = driver.find_elements(By.XPATH, "//div[contains(@class, 'MuiStack-root css-kvb41a')]")
+    for autor in encontrar_autores:
         # Obtener todos los elementos <span> dentro del contenedor de autores y concatenarlos
         autores_texto = " | ".join([span.text for span in autor.find_elements(By.TAG_NAME, "span")])
         autores.append(autores_texto)
 
     # Verificador de longitud de listas
     if not (len(titulos) == len(anios) == len(autores)):
-        print("Error: Las cantidades de títulos, años y autores no coinciden. Revisar los selectores.")
+        print("Error: Las cantidades de titulos, anios y autores no coinciden. Revisar los selectores.")
         break
 
     # Ir a la siguiente pagina
     try:
-        next_button = driver.find_element(By.XPATH, "//button[@aria-label='Go to next page']")
-        next_button.click()
+        pagina_sig = driver.find_element(By.XPATH, "//button[@aria-label='Go to next page']")
+        pagina_sig.click()
         time.sleep(5)  
     except:
         print("No hay mas paginas...")
